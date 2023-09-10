4c0a5a295951e935c728f5f3b5dd173248e7f1b2
Henry <henryjrose2@gmail.com>
2023-09-10 15:05:32 -0400

    Made Conceptual Floor

--- conceptualization.java
+++ conceptualization.java
@@ -1,3 +1,34 @@
 public class conceptualization {
-    
+ 
+    public static void main(String[] args){
+        String[][] floor = createFloor(7, 7);
+        printMatrix(floor);
+    }
+
+    public static String[][] createFloor(int x, int y){
+        if(x <= 0 || y <= 0){
+            return null;
+        }
+        String[][] floor = new String[x][y];
+        for(int i = 0; i < floor.length; i++){
+            for(int j = 0; j < floor[0].length; j++){
+                floor[i][j] = "□";
+            }
+        }
+        return floor;
+    }
+
+    public static void printMatrix(Object[][] in){
+        for(int i = 0; i < in.length; i++){
+            for(int j = 0; j < in[0].length; j++){
+                System.out.print(in[i][j]);
+            }
+            if(i < in.length - 1){
+                System.out.print("\n");
+            }
+        }
+
+
+    }
+
 }
